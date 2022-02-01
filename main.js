f6a60c8a5ee66f8cbc77f29a4cf9ac931f2559d8
RaissaMoreira <raissamoreira80@gmail.com>
2022-02-01 17:13:26 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,108 @@
+// abre e fecha o menu quando clicar no ícone
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for (const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show');
+  });
+};
+
+// quando clicar em um item do menu, esconder o menu
+const links = document.querySelectorAll('nav ul li a');
+
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show');
+  });
+};
+
+// mudar o header quando der scroll
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+function changeHeaderWhenScroll () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll');
+  } else {
+    header.classList.remove('scroll');
+  }
+};
+
+ // Testimonials carousel slider swiper
+ const swiper = new Swiper('.swiper-container', {
+   slidesPerView: 1,
+   pagination: {
+    el: '.swiper-pagination'
+  },
+   mousewheel: true,
+   keyboard: true,
+   breakpoints: {
+     767: {
+       slidesPerView: 2,
+       setWrapperSize: true
+     }
+   }
+});
+
+// ScrollReveal: mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+// Botão voltar para o topo
+function backToTop () {
+  const backToTopButton = document.querySelector('.back-to-top');
+
+  if(window.scrollY >= 560) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+};
+
+// Menu ativo consforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// when scroll
+window.addEventListener('scroll', () => {
+  changeHeaderWhenScroll ();
+  backToTop ();
+  activateMenuAtCurrentSection ();
+});
